8fd1151d2f31fe7ed909d8eaa70aa9d6b6897e91
OlgaArenas22 <olga.arenas.rosario@alumnos.upm.es>
2026-01-03 12:53:11 +0100

    Modified Result

--- src/Repository/ResultRepository.php
+++ src/Repository/ResultRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Result;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -11,5 +12,30 @@ class ResultRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Result::class);
+    }
+
+    /**
+     * @return Result[]
+     */
+    public function findByUser(User $user, string $orderBy = 'id'): array
+    {
+        $allowed = [ 'id', 'result', 'time' ];
+        if (!in_array($orderBy, $allowed, true)) {
+            $orderBy = 'id';
+        }
+
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.user = :u')
+            ->setParameter('u', $user)
+            ->orderBy('r.' . $orderBy, 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findOneById(int $id): ?Result
+    {
+        /** @var Result|null $result */
+        $result = $this->find($id);
+        return $result;
     }
 }
